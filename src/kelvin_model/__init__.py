34864fbcb21ee9b9eabcdefaf7bdba63081b210b
Antoine Weisrock <antoine.weisrock@gmail.com>
2025-05-09 16:32:54 +0200

    build: make main functions of the module importable

--- src/kelvin_model/__init__.py
+++ src/kelvin_model/__init__.py
@@ -1,5 +1,9 @@
 # coding: utf-8
 
+from .compute_stress import compute_stress
+from .image_correlation import image_correlation
+from .optimize_diagonals import optimize_diagonals
+
 import importlib.util
 
 kelvin_lib_path = importlib.util.find_spec("kelvin_model.kelvin_lib").origin
--- src/microstructure_orientation/__init__.py
+++ src/microstructure_orientation/__init__.py
@@ -1,1 +1,7 @@
 # coding: utf-8
+
+from .exposure_fusion import exposure_fusion
+from .gabor_filter import apply_gabor_filter
+from .gaussian_fit import gaussian_fit
+from .make_plots import make_plots
+from .peak_detection import detect_peaks
