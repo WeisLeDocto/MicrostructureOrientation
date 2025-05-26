49eff765abc85ccbb4b5fdce70cf3504cde86351
Antoine Weisrock <antoine.weisrock@gmail.com>
2025-05-26 11:11:06 +0200

    ref: group arguments in single arrays for better memory management

--- src/kelvin_model/include/kelvin_lib.hpp
+++ src/kelvin_model/include/kelvin_lib.hpp
@@ -8,97 +8,34 @@
 extern "C" {
 #endif
 
-EXPORT_SYMBOL void calc_stress(double exx,
-                               double eyy,
-                               double exy,
-                               double lamh,
-                               double lam11,
-                               double lam21,
-                               double lam31,
-                               double lam41,
-                               double lam51,
-                               double lam12,
-                               double lam22,
-                               double lam32,
-                               double lam42,
-                               double lam52,
-                               double lam13,
-                               double lam23,
-                               double lam33,
-                               double lam43,
-                               double lam53,
-                               double lam14,
-                               double lam24,
-                               double lam34,
-                               double lam44,
-                               double lam54,
-                               double lam15,
-                               double lam25,
-                               double lam35,
-                               double lam45,
-                               double lam55,
-                               double val1,
-                               double val2,
-                               double val3,
-                               double val4,
-                               double val5,
-                               double theta_1,
-                               double theta_2,
-                               double theta_3,
-                               double sigma_1,
-                               double sigma_2,
-                               double sigma_3,
-                               double density,
-                               double* sxx,
-                               double* syy,
-                               double* sxy);
+typedef struct {
+    double exx;
+    double eyy;
+    double exy;
+    double theta_1;
+    double theta_2;
+    double theta_3;
+    double sigma_1;
+    double sigma_2;
+    double sigma_3;
+    double density;
+} PixelData;
 
-EXPORT_SYMBOL void calc_stresses(double* exx,
-                                 double* eyy,
-                                 double* exy,
-                                 double lamh,
-                                 double lam11,
-                                 double lam21,
-                                 double lam31,
-                                 double lam41,
-                                 double lam51,
-                                 double lam12,
-                                 double lam22,
-                                 double lam32,
-                                 double lam42,
-                                 double lam52,
-                                 double lam13,
-                                 double lam23,
-                                 double lam33,
-                                 double lam43,
-                                 double lam53,
-                                 double lam14,
-                                 double lam24,
-                                 double lam34,
-                                 double lam44,
-                                 double lam54,
-                                 double lam15,
-                                 double lam25,
-                                 double lam35,
-                                 double lam45,
-                                 double lam55,
-                                 double val1,
-                                 double val2,
-                                 double val3,
-                                 double val4,
-                                 double val5,
-                                 double* theta_1,
-                                 double* theta_2,
-                                 double* theta_3,
-                                 double* sigma_1,
-                                 double* sigma_2,
-                                 double* sigma_3,
-                                 double* density,
-                                 int rows,
-                                 int cols,
-                                 double* sxx,
-                                 double* syy,
-                                 double* sxy);
+typedef struct {
+    double lambda_h;
+    double lambda_11, lambda_21, lambda_31, lambda_41, lambda_51;
+    double lambda_12, lambda_22, lambda_32, lambda_42, lambda_52;
+    double lambda_13, lambda_23, lambda_33, lambda_43, lambda_53;
+    double lambda_14, lambda_24, lambda_34, lambda_44, lambda_54;
+    double lambda_15, lambda_25, lambda_35, lambda_45, lambda_55;
+    double val1, val2, val3, val4, val5;
+} LambdaParams ;
+
+EXPORT_SYMBOL void calc_stresses(const PixelData* input,
+                                 const LambdaParams* params,
+                                 const int rows,
+                                 const int cols,
+                                 double* stress);
 
 #ifdef __cplusplus
 }
