118468052cf102e92bdcc63daacb1d685f3817e0
Antoine Weisrock <antoine.weisrock@gmail.com>
2025-10-09 11:31:10 +0200

    fix: forgot to update code in previous commit

--- figures/real.py
+++ figures/real.py
@@ -103,8 +103,9 @@ def process_img(img: np.ndarray,
 
     sign_3 = cp.tile(cp.linspace(0, cp.pi, NB_ANGLES),
                      (img.shape[0], img.shape[1], 3, 1))
-    sign_3 = amp * cp.exp(-cp.power((cp.mod(sign_3 + cp.pi / 2 - ang, cp.pi)
-                                     - cp.pi / 2) / sig, 2))
+    sign_3 = amp * cp.exp(-cp.power(cp.atan2(cp.sin(2 * (sign_3 - ang)),
+                                             cp.cos(2 * (sign_3 - ang)))
+                                    / (4 * sig), 2))
     signal = cp.sum(sign_3, axis=2)
     del ang
 
