b3b526d385c689a796d5646ab780657179577c3e
Antoine Weisrock <antoine.weisrock@gmail.com>
2025-10-14 11:30:10 +0200

    fix: angle already converted to radians

--- src/kelvin_model/_kelvin_utils.py
+++ src/kelvin_model/_kelvin_utils.py
@@ -67,9 +67,9 @@ def prepare_data(ref_img: np.ndarray,
     sigma_1 = gauss_fit[..., 0]
     sigma_2 = np.nan_to_num(gauss_fit[..., 2])
     sigma_3 = np.nan_to_num(gauss_fit[..., 4])
-    theta_1 = np.deg2rad(peaks[..., 0])
-    theta_2 = np.deg2rad(np.nan_to_num(peaks[..., 1]))
-    theta_3 = np.deg2rad(np.nan_to_num(peaks[..., 2]))
+    theta_1 = peaks[..., 0]
+    theta_2 = np.nan_to_num(peaks[..., 1])
+    theta_3 = np.nan_to_num(peaks[..., 2])
 
     m_1 = np.cos(2 * theta_1)
     m_2 = np.cos(2 * theta_2)
