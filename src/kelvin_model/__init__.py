a30e97e86291b0770c4f05234a5bfdb85691b9f7
Antoine Weisrock <antoine.weisrock@gmail.com>
2025-12-08 15:43:23 +0100

    feat: provide access to protected members for figure generation

--- src/kelvin_model/__init__.py
+++ src/kelvin_model/__init__.py
@@ -4,6 +4,8 @@ from .compute_stress import compute_stress
 from .image_correlation import image_correlation
 from .results_vs_expe import compare_results_expe
 from .optimize_common import optimize_diagonals
+from ._kelvin_utils import (prepare_data, calc_density, stress_diag_to_force,
+                            diagonals_interpolator)
 
 import importlib.util
 
